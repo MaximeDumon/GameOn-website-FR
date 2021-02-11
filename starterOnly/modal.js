d099d3235aaed5cd0bab41c503385191751c4d90
Maxime Dumon <maximedumon@pm.me>
2021-02-11 16:58:25 +0100

    Ajout de la fermeture du formulaire + corrections HTML

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -9,15 +9,20 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBtn = document.getElementById("modal-btn");
+const closeModalBtn = document.getElementById("close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.addEventListener("click", launchModal);
+closeModalBtn.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// Close modal form without saving data when clicking on the 'close' button
+function closeModal() {
+  modalbg.style.display = "none";
+}
