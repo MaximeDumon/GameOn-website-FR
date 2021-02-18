9e840a8d49a128e58bf65ad3abe336126fdda533
Maxime Dumon <maximedumon@pm.me>
2021-02-18 10:30:34 +0100

    * Suppression de la classe topNav au profit de l'id myTopNav étant donné que la classe n'était jamais réutilisée dans le code HTML. * Refactoring du code CSS pour le survol des élements de navigation et de l'élément actif. * Suppression du doublon @media pour les résolutions inférieures à 768 px. * Suppression de la balise de navigation "accueil" qui n'est pas présente sur la maquette. * Changement du breakpoint mobile à 1024 px car le design casse à ce moment là avec la barre de navigation qui passe sous le logo. * Ajout d'une fonction de validation pour les champs du formulaire et modification du HTML et CSS pour afficher un message d'erreur lorsque l'on ne respecte pas le format attendu. (WIP).

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,10 @@
+// @ts-check
+
+/** Object describing the inscription form and its fields */
+class InscriptionForm {
+  constructor() {}
+}
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -11,18 +18,92 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.getElementById("modal-btn");
 const closeModalBtn = document.getElementById("close");
-const formData = document.querySelectorAll(".formData");
 
-// launch modal event
+const inputFields = document.querySelectorAll(".formData > input");
+
+// Launch modal event
 modalBtn.addEventListener("click", launchModal);
+// Close modal event
 closeModalBtn.addEventListener("click", closeModal);
 
-// launch modal form
+// Add an event listener for each input field of the form
+for (let i = 0; i < inputFields.length; i++) {
+  inputFields[i].addEventListener("change", validateField);
+}
+
+// Launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
 // Close modal form without saving data when clicking on the 'close' button
 function closeModal() {
   modalbg.style.display = "none";
+}
+
+/* Function validate called when submitting the form, it calls the validateField() function for every field */
+function validate() {
+  let formValid = true;
+  inputFields.forEach((element) => {
+    formValid &&= validateField(element.dispatchEvent(new Event("change")));
+    if (!formValid) {
+      console.log("Form not valid!");
+    }
+  });
+}
+
+/* Function that validates the field value that has been changed and displays a proper error message if not ok */
+function validateField(e) {
+  let errorMessage = "";
+  let regEx;
+  let fieldValid = true;
+  const value = e.target.value;
+  switch (e.target.id) {
+    case "first":
+      regEx = /a/;
+      if (!regEx.test(value)) {
+        errorMessage = "Veuillez saisir votre prénom (2 caractères minimum).";
+        fieldValid = false;
+      }
+      break;
+    case "last":
+      regEx = /a/;
+      if (!regEx.test(value)) {
+        errorMessage = "Veuillez saisir votre nom.";
+        fieldValid = false;
+      }
+      break;
+    case "email":
+      regEx = /a/;
+      if (!regEx.test(value)) {
+        errorMessage =
+          "Veuillez saisir une adresse email au format <texte>@<texte>.<texte>";
+        fieldValid = false;
+      }
+      break;
+    case "birthdate":
+      regEx = /a/;
+      if (!regEx.test(value)) {
+        errorMessage = "Veuillez saisir votre date de naissance.";
+        fieldValid = false;
+      }
+      break;
+    case "quantity":
+      if (value < 0) {
+        errorMessage = "La quantité ne peut être inférieure à 0.";
+        fieldValid = false;
+      }
+      break;
+    case "checkbox1":
+      if (!value) {
+        errorMessage =
+          "Veuillez accepter les conditions générales d'utilisation.";
+        fieldValid = false;
+      }
+      break;
+    default:
+      console.log("Wrong field has been selected!");
+  }
+  // <this> is the <input> element which fired the event, the error message is right next to it in the DOM.
+  this.nextElementSibling.innerHTML = errorMessage;
+  return fieldValid;
 }
